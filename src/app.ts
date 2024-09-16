2e82025e767541476ffb88c140a0628f441feb6e
Camilla Mengozzi <camillamengozzi94@gmail.com>
2024-09-16 02:13:03 +0200

    aggiorna

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,7 @@ import * as orm from "./orm";
 const mainOrm = async () => {
     //console.log(await orm.insertPost("Le spiagge più belle d'Europa", "Spiagge da sogno per le vacanze estive"));
     //console.log(await orm.getPosts());
-    //console.log(await orm.updatePosts("66e7647ac40a1f0243a1a15d"));
+    //console.log(await orm.updatePosts("66e7647ac40a1f0243a1a15d", false));
     console.log(await orm.deletePost("66e7647ac40a1f0243a1a15d"));
     
 }
